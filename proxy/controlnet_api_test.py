314b8b37476af5f288115553fbaaa78dca7af9f5
Sandy Corsillo <the_dusky@icloud.com>
2023-11-02 15:53:24 -0400

    controlnet 2 step

--- proxy/controlnet_api_test.py
+++ proxy/controlnet_api_test.py
@@ -46,6 +46,5 @@ payload = {
 response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
 
 r = response.json()
-print(r)
 image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
 image.save('cnet_output.png')
