c6f57056cba4218dd09dede3e3dae649fb41919a
Sandy Corsillo <the_dusky@icloud.com>
2023-11-10 15:12:39 -0500

    fixed some errors

--- setup.sh
+++ setup.sh
@@ -131,16 +131,16 @@ start_a1111() {
         case $i in
             1.5) 
                 echo "Starting Auto1111 for 1.5"
-                runuser -l ubuntu -c 'pm2 start --name auto1111_1.5 "./webui.sh -p 3115"';;
+                runuser -l ubuntu -c 'cd /home/ubuntu/auto1111 && pm2 start --name auto1111_1.5 "./webui.sh -p 3115"';;
             2.1)
                 echo "Starting Auto1111 for 2.1"
-                runuser -l ubuntu -c 'pm2 start --name auto1111_2.1 "./webui.sh -p 3121"';;
+                runuser -l ubuntu -c 'cd /home/ubuntu/auto1111 && pm2 start --name auto1111_2.1 "./webui.sh -p 3121"';;
             3.0)
                 echo "Starting Auto1111 for 3.0"
-                runuser -l ubuntu -c 'pm2 start --name auto1111_3.0 "./webui.sh -p 3130"';;
+                runuser -l ubuntu -c 'cd /home/ubuntu/auto1111 && pm2 start --name auto1111_3.0 "./webui.sh -p 3130"';;
             SDXL)
                 echo "Starting Auto1111 for SDXL"
-                runuser -l ubuntu -c 'pm2 start --name auto1111_3.0 "./webui.sh -p 3130"';;
+                runuser -l ubuntu -c 'cd /home/ubuntu/auto1111 && pm2 start --name auto1111_3.0 "./webui.sh -p 3130"';;
             *)
                 echo "$i is an Invalid option";;
         esac
