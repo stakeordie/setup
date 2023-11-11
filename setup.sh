6f93ff2fd06b16bd22ae777c90125c985c04a8ca
Sandy Corsillo <the_dusky@icloud.com>
2023-11-11 16:30:55 -0500

    added refiner

--- setup.sh
+++ setup.sh
@@ -104,6 +104,7 @@ install_a1111() {
         mkdir -p /home/ubuntu/checkpoints/
         cd /home/ubuntu/checkpoints/
         wget --user $HUGGING_USER --password $HUGGING_PASSWORD https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
+        wget --user $HUGGING_USER --password $HUGGING_PASSWORD https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
         wget --user $HUGGING_USER --password $HUGGING_PASSWORD https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.ckpt
         wget --user $HUGGING_USER --password $HUGGING_PASSWORD https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt
     fi
