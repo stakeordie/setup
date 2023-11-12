79bfa10578f549a98cf0f5f22b509e7251edcff3
Sandy Corsillo <the_dusky@icloud.com>
2023-11-12 14:52:19 -0500

    work on api_test

--- proxy/api_test.py
+++ proxy/api_test.py
@@ -14,7 +14,7 @@ payload = {
     "height": 1024,
     "width": 1024,
     "negative_prompt": "((photo)), ((photograph)), trees, nature, text, lines of text, frame, frames, border, title, words, print",
-    "prompt": "Oragami Diorama. Perspective from above, godlike, tilt shift, the city is a toy. Looking down through clouds. Blimps fly below. A (broken down) ((crumbling)) empty high-rise city like New York. in winter, snow storm, cold, ice, deep freeze. it is the middle of the day, bright, sunshine. the sky is an electric rainbow of colors. A massive ((explosion)) in the foreground on a section of the seawall that towers over everything. Water pours into the city beginning the flood that will be its certain demise.",
+    "prompt": "Cut Paper Oragami Sculpture. Perspective from above, godlike, tilt shift, the city is a toy. Looking down through clouds. Blimps fly below. A (broken down) ((crumbling)) empty high-rise city like New York. in winter, snow storm, cold, ice, deep freeze. it is the middle of the day, bright, sunshine. the sky is an electric rainbow of colors. A massive ((explosion)) in the foreground on a section of the seawall that towers over everything. Water pours into the city beginning the flood that will be its certain demise.",
     "sampler_name": "DPM++ 2M",
     "steps": 20
 }
