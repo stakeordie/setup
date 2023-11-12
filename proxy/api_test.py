5e223a61ba51bb4d415cbba57e91367259aacabf
Sandy Corsillo <the_dusky@icloud.com>
2023-11-12 16:18:38 -0500

    work on api_test

--- proxy/api_test.py
+++ proxy/api_test.py
@@ -6,7 +6,16 @@ import argparse
 from PIL import Image
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-m", "--nmethod", help="GPU or CPU", default="GPU")
+parser.add_argument("-cfg", "--cfg-scale", help="CFG Scale (0 - Unlimited, 7.5 standard)", default="7.5")
+parser.add_argument("-h", "--height", help="Height in Pixels", default="1024")
+parser.add_argument("-w", "--width", help="Width in Pixels", default="1024")
+parser.add_argument("-p", "--prompt", help="Prompt", default="an SNES Video Game Scene. Perspective from above, godlike, tilt shift, the city is a toy. Looking down through clouds. Blimps fly below. A (broken down) ((crumbling)) empty high-rise city like New York. in winter, snow storm, cold, ice, deep freeze. it is the middle of the day, bright, sunshine. . A massive ((explosion)) in the foreground on a section of the seawall that towers over everything. Water pours into the city beginning the flood that will be its certain demise.")
+parser.add_argument("-np", "--negative-prompt", help="Negative Prompt", default="((photo)), ((photograph)), trees, nature, text, lines of text, frame, frames, border, title, words, print")
+parser.add_argument("-s", "--steps", help="Steps (0 - 50)", default="20")
+parser.add_argument("-sd", "--seed", help="Stable Diffusion Seed (-1 = Random)", default="754943641")
+parser.add_argument("-sn", "--sampler-name", help="Sampler Name", default="DPM++ 2M")
+parser.add_argument("-rnm", "--random-noise-method", help="GPU or CPU", default="GPU")
+
 args = parser.parse_args()
 def load_input_image(path):
     with open(path, 'rb') as file:
@@ -14,14 +23,14 @@ def load_input_image(path):
 
 url = "http://127.0.0.1:3000/sdapi/v1/txt2img"
 payload = {
-    "cfg_scale": 7,
-    "height": 1024,
-    "width": 1024,
-    "negative_prompt": "((photo)), ((photograph)), trees, nature, text, lines of text, frame, frames, border, title, words, print",
-    "prompt": "an SNES Video Game Scene. Perspective from above, godlike, tilt shift, the city is a toy. Looking down through clouds. Blimps fly below. A (broken down) ((crumbling)) empty high-rise city like New York. in winter, snow storm, cold, ice, deep freeze. it is the middle of the day, bright, sunshine. . A massive ((explosion)) in the foreground on a section of the seawall that towers over everything. Water pours into the city beginning the flood that will be its certain demise.",
-    "sampler_name": "DPM++ 2M",
-    "steps": 20,
-    "seed": "754943641",
+    "cfg_scale": args.cfg_scale,
+    "height": args.height,
+    "width": args.width,
+    "negative_prompt": args.negative_prompt,
+    "prompt": args.prompt,
+    "sampler_name": args.sampler_name,
+    "steps": args.steps,
+    "seed": args.seed,
     "override_settings": {
       "randn_source": args.nmethod,
     },
