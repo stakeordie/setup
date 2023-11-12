9eb9986ed7438ec2c52e5e54ef4abef83b042cf4
Sandy Corsillo <the_dusky@icloud.com>
2023-11-12 13:37:35 -0500

    work on api_test

--- proxy/api_test.py
+++ proxy/api_test.py
@@ -8,7 +8,7 @@ def load_input_image(path):
     with open(path, 'rb') as file:
         return base64.b64encode(file.read()).decode()
 
-url = "tcp://0.0.0.0:8100"
+url = "http://0.0.0.0:8100"
 payload = {
   "input": {
     "prompt": "Hello World"
