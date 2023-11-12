b9e9a4f875d2544ac5dc5b6492f07438a43a8e45
Sandy Corsillo <the_dusky@icloud.com>
2023-11-12 16:25:17 -0500

    work on api_test

--- proxy/api_test.py
+++ proxy/api_test.py
@@ -6,22 +6,23 @@ import argparse
 from PIL import Image
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-cfg", "--cfg-scale", help="CFG Scale (0 - Unlimited, 7.5 standard)", default="7.5")
+parser.add_argument("-p", "--port", help="Instance Port", default="3100")
+parser.add_argument("--cfg-scale", help="CFG Scale (0 - Unlimited, 7.5 standard)", default="7.5")
 parser.add_argument("--height", help="Height in Pixels", default="1024")
 parser.add_argument("--width", help="Width in Pixels", default="1024")
-parser.add_argument("-p", "--prompt", help="Prompt", default="an SNES Video Game Scene. Perspective from above, godlike, tilt shift, the city is a toy. Looking down through clouds. Blimps fly below. A (broken down) ((crumbling)) empty high-rise city like New York. in winter, snow storm, cold, ice, deep freeze. it is the middle of the day, bright, sunshine. . A massive ((explosion)) in the foreground on a section of the seawall that towers over everything. Water pours into the city beginning the flood that will be its certain demise.")
-parser.add_argument("-np", "--negative-prompt", help="Negative Prompt", default="((photo)), ((photograph)), trees, nature, text, lines of text, frame, frames, border, title, words, print")
-parser.add_argument("-s", "--steps", help="Steps (0 - 50)", default="20")
-parser.add_argument("-sd", "--seed", help="Stable Diffusion Seed (-1 = Random)", default="754943641")
-parser.add_argument("-sn", "--sampler-name", help="Sampler Name", default="DPM++ 2M")
-parser.add_argument("-rnm", "--random-noise-method", help="GPU or CPU", default="GPU")
+parser.add_argument("--prompt", help="Prompt", default="an SNES Video Game Scene. Perspective from above, godlike, tilt shift, the city is a toy. Looking down through clouds. Blimps fly below. A (broken down) ((crumbling)) empty high-rise city like New York. in winter, snow storm, cold, ice, deep freeze. it is the middle of the day, bright, sunshine. . A massive ((explosion)) in the foreground on a section of the seawall that towers over everything. Water pours into the city beginning the flood that will be its certain demise.")
+parser.add_argument("--negative-prompt", help="Negative Prompt", default="((photo)), ((photograph)), trees, nature, text, lines of text, frame, frames, border, title, words, print")
+parser.add_argument("--steps", help="Steps (0 - 50)", default="20")
+parser.add_argument("--seed", help="Stable Diffusion Seed (-1 = Random)", default="754943641")
+parser.add_argument("--sampler-name", help="Sampler Name", default="DPM++ 2M")
+parser.add_argument("--random-noise-method", help="GPU or CPU", default="GPU")
 
 args = parser.parse_args()
 def load_input_image(path):
     with open(path, 'rb') as file:
         return base64.b64encode(file.read()).decode()
 
-url = "http://127.0.0.1:3000/sdapi/v1/txt2img"
+url = f'http://127.0.0.1:${port}/sdapi/v1/txt2img'
 payload = {
     "cfg_scale": args.cfg_scale,
     "height": args.height,
